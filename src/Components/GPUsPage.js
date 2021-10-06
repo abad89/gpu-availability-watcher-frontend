76593ed5bd6fa4dad1b3c24bfe867626616aab59
abad89 <abadalamenti89@gmail.com>
2021-10-05 20:30:38 -0400

    move gpu list to GPUsPage component

---
+++ src/Components/GPUsPage.js
@@ -0,0 +1,32 @@
+import React from "react";
+import GPUCard from "./GPUCard";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL
+
+export default function GPUsPage() {
+  const [gpuList, setGpuList] = useState([]);
+
+  useEffect(() => {
+    fetch(BASE_URL + "/gpus")
+      .then((r) => r.json())
+      .then(setGpuList);
+  }, []);
+
+  let activeGPUS = gpuList.filter(function (gpu) {
+    return gpu.url != null;
+  });
+
+  const gpusItem = activeGPUS.map((gpu) => (
+    <GPUCard
+      key={gpu.id}
+      name={gpu.name}
+      largeImage={gpu.largeImage}
+      salePrice={gpu.salePrice}
+      onlineAvailability={gpu.onlineAvailability}
+      url={gpu.url}
+      sku={gpu.sku}
+    />
+  ));
+
+  return <div>{gpusItem}</div>;
+}
