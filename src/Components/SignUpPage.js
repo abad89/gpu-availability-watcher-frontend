6c395c07913e1dd05eab8bd3456f3e4447e5917c
abad89 <abadalamenti89@gmail.com>
2022-02-06 23:16:50 -0500

    add email disclaimer, favicon, and page title

--- src/Components/SignUpPage.js
+++ src/Components/SignUpPage.js
@@ -33,10 +33,10 @@ export default function SignUpPage() {
     });
     const data = await response.json();
     if (response.ok) {
-      localStorage.setItem("token", data.jwt)
-      window.location.reload()
+      localStorage.setItem("token", data.jwt);
+      window.location.reload();
     } else {
-      console.log(data)
+      console.log(data);
       setErrors(data.errors);
     }
   }
@@ -44,7 +44,7 @@ export default function SignUpPage() {
   return (
     <div className="">
       <div className="d-flex p-2 justify-content-center">
-      <p>Sign up:</p>
+        <p>Sign up:</p>
       </div>
       <div className={"d-flex p-2 justify-content-center"}>
         <form onSubmit={handleSubmit}>
@@ -54,8 +54,8 @@ export default function SignUpPage() {
             name="email"
             value={formData.email}
             onChange={handleChange}
-            ></input>
-          <br/>
+          ></input>
+          <br />
           <input
             className="form-control"
             type="password"
@@ -63,14 +63,24 @@ export default function SignUpPage() {
             name="password"
             value={formData.password}
             onChange={handleChange}
-            ></input>
-          <br/>
-          <button className={"btn btn-primary mx-auto d-block"} type="submit" >Register</button>
+          ></input>
+          <br />
+          <button className={"btn btn-primary mx-auto d-block"} type="submit">
+            Register
+          </button>
         </form>
       </div>
+      <div>
+        <p className=" text-center text-info">
+          Email system currently being tested. <br /> By signing up you agree to
+          potentially receive spam emails from me.
+        </p>
+      </div>
       <div className="d-flex flex-column p-2 justify-content-center text-center">
-        <p>{errors}</p>
-        <p>Already have an account? <Link to="/">Log in.</Link></p>
+        <p className="text-danger">{errors}</p>
+        <p>
+          Already have an account? <Link to="/">Log in.</Link>
+        </p>
       </div>
     </div>
   );
